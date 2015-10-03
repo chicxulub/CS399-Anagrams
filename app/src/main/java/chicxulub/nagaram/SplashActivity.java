55b871d15a06a18c1163ae48632f665b73f22fe7
Emily Bartman <bartman@nau.edu>
2015-10-03 14:07:07 -0700

    Added animations for fun

--- app/src/main/java/chicxulub/nagaram/SplashActivity.java
+++ app/src/main/java/chicxulub/nagaram/SplashActivity.java
@@ -1,41 +1,58 @@
 package chicxulub.nagaram;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements View.OnClickListener {
+    private Intent aboutIntent;
+    private Intent playIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final Button about = (Button) findViewById(R.id.action_about);
+        final Button play = (Button) findViewById(R.id.play);
+
+        this.playIntent = new Intent(this, ChooseDifficulty.class);
+        this.aboutIntent = new Intent(this, AboutNagaram.class);
+
+        about.setOnClickListener(this);
+        play.setOnClickListener(this);
+
+        // rock the image
+        final ImageView animationTarget = (ImageView) this.findViewById(R.id.imageView);
+        final Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+
+        new Thread(new Runnable() {
+            public void run() {
+                animationTarget.startAnimation(animation);
+            }
+        }).start();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
+    public void onClick(View v) {
+        SplashActivity activity = this;
+        switch(v.getId()){
+            case R.id.play:
+                startActivity(activity.playIntent);
+                break;
+            case R.id.action_about:
+                startActivity(activity.aboutIntent);
+                break;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_about) {
-            // start new activity
-            Intent aboutIntent = new Intent(this, AboutNagaram.class);
-            startActivity(aboutIntent);
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
