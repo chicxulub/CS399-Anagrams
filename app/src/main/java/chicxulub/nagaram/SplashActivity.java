46a8320047254b08fb5839f58bc5a42a925ffbc1
Emily Bartman <bartman@nau.edu>
2015-10-03 15:01:41 -0700

    Added difficulty activity

--- app/src/main/java/chicxulub/nagaram/SplashActivity.java
+++ app/src/main/java/chicxulub/nagaram/SplashActivity.java
@@ -1,6 +1,7 @@
 package chicxulub.nagaram;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
