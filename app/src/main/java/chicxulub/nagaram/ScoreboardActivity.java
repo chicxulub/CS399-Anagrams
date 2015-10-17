4c08f7673e17de014d04a4fa92a1c59876241700
Emily Bartman <bartman@nau.edu>
2015-10-16 20:54:31 -0700

    Added home intent to scoreboard

--- app/src/main/java/chicxulub/nagaram/ScoreboardActivity.java
+++ app/src/main/java/chicxulub/nagaram/ScoreboardActivity.java
@@ -1,5 +1,6 @@
 package chicxulub.nagaram;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,21 +11,29 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class ScoreboardActivity extends AppCompatActivity {
+public class ScoreboardActivity extends AppCompatActivity implements View.OnClickListener {
     private SavedDataHelper saveData;
+    private Intent homeIntent;
     ScoreAdapter scoreAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboard);
+
+        homeIntent = new Intent(this, SplashActivity.class);
+
         saveData = saveData.getInstance(this);
+
+        ImageButton goHome = (ImageButton)findViewById(R.id.goHome);
+        goHome.setOnClickListener(this);
 
         List<Score> all = saveData.all(this);
         if (all.size() > 0)
@@ -38,5 +47,10 @@ public class ScoreboardActivity extends AppCompatActivity {
         Log.d(getClass().getSimpleName(), String.valueOf(listView));
         listView.setAdapter(scoreAdapter);
         System.out.println("finished");
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(homeIntent);
     }
 }
